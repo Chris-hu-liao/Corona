beab8de7d7b2a701d20084919d9372bf1d998c39
Chris-hu-liao <66919955+Chris-hu-liao@users.noreply.github.com>
2020-06-19 12:00:02 -0400

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,16 @@
+print("hello world")
+
+name=input("Best country in the world: ")
+print(name)
+
+name=name.lower()
+if (name=="Canada"):
+    print(name +"is the best")
+elif  (name=="Norway"):
+    print(name.Lower()+"is the second best")
+else:
+    print("BOOO WRONG ANSWER")
+
+fruits=['apple, grannysmtih, gale','banana','peach, sweet,not sweet','orange,mandarin,bloodrange','banana,big,small']
+for fruit in fruits:
+          print(fruit)
