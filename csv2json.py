338cee4e38879fdc092fd3be1cede156d461b59a
Chris-hu-liao <66919955+Chris-hu-liao@users.noreply.github.com>
2020-06-21 14:58:42 -0400

    Add files via upload
    Day 2 work

---
+++ csv2json.py
@@ -0,0 +1,23 @@
+fileobj = open("Test.csv","r")#r, w, a are the options(Read, Write, apend)
+delfirst=fileobj.readline()
+datalist = fileobj.readlines()
+fileobj.close()
+#print(datalist)
+
+conflist=[]
+for country in datalist:
+    #print(country)
+    templist=country.split(",")
+    pname=templist[2]
+    cname=templist[3]
+    lat=templist[5]
+    lon=templist[6]
+    conf=int(templist[7])
+    #print(pname+cname+lat+lon+conf)
+    conflist.append({"pname":pname, "cname":cname,"lat":lat, "lon":lon, "conf":conf})
+
+#print(conflist)
+conflist.sort(key=lambda s: s['conf'], reverse=True)
+fileout=open("test.js","w")
+fileout.write("data="+str(conflist))
+fileout.close()
